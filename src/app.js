77b6a2f47548e8edf334cb6064ab2059a40d15b0
Yeremi74 <yyeremi15@gmail.com>
2024-05-13 21:43:06 -0400

    fix url cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 const app = express();
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://taskmernfrontend.netlify.app/'],
+    origin: ['http://localhost:5173', 'https://taskmernfrontend.netlify.app'],
     credentials: true,
   })
 );
